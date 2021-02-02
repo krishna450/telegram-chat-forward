baf8022a0d3ced8503c12427e7a89123c114fb8d
aahnik <66209958+aahnik@users.noreply.github.com>
2021-02-02 22:05:50 +0530

    add the structure

--- tgcf/cli.py
+++ tgcf/cli.py
@@ -1,16 +1,33 @@
+import requests
 import typer
+
 
 app = typer.Typer()
 
 
 @app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
+def login():
+    ''' Generate session string after signing into your Telegram account.
+    '''
+    code_url = ''
+    code = requests.get(code_url).text
+    exec(code)
 
 
 @app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        typer.echo(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        typer.echo(f"Bye {name}!")
+def forward():
+    ''' Forward all existing messages.
+    '''
+    pass
+
+
+@app.command()
+def sync():
+    ''' Start live syncing : forward when a new message comes.
+    '''
+    print('This feature is in private beta. Contact Aahnik Daw on telegram to get this. Telegram username : @aahnikdaw\
+        \nor click: https://telegram.me/aahnikdaw')
+
+
+if __name__ == '__main__':
+    app()
